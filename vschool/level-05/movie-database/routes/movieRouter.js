7ac6cbe6e9c3d409bf46c14d214da9fb461ba4bb
Jacob Nevitt <jacobcodes13@gmail.com>
2023-09-21 09:05:18 -0600

    Added all CRUD operations to backend

--- vschool/level-05/movie-database/routes/movieRouter.js
+++ vschool/level-05/movie-database/routes/movieRouter.js
@@ -2,6 +2,17 @@ const express = require("express")
 const Movie = require("../models/movie")
 const movieRouter = express.Router()
 
+// GET One //
+movieRouter.get("/", (req, res, next) => {
+  Movie.find((err, movies) => {
+    if (err) {
+      res.status(500)
+      return next(err)
+    }
+    return res.status(200).send(movies)
+  })
+})
+
 // POST One //
 movieRouter.post("/", (req, res, next) => {
   const newMovie = new Movie(req.body)
@@ -10,8 +21,35 @@ movieRouter.post("/", (req, res, next) => {
       res.status(500)
       return next(err)
     }
-    return res.status(201).esnd(savedMovie)
+    return res.status(201).send(savedMovie)
   })
+})
+
+// DELETE One //
+movieRouter.delete("/:movieId", (req, res, next) => {
+  Movie.findOneAndDelete({ _id: req.params.movieId }, (err, removedMovie) => {
+    if (err) {
+      res.status(500)
+      return next(err)
+    }
+    return res.status(200).send(`Removed the movie ${removedMovie.title} from the database!`)
+  })
+})
+
+// UPDATE One //
+movieRouter.put("/:movieId", (req, res, next) => {
+  Movie.findOneAndUpdate(
+    { _id: req.params.movieId },
+    req.body,
+    { new: true },
+    (err, updatedMovie) => {
+      if (err) {
+        res.status(500)
+        return next(err)
+      }
+      return res.status(201).send(updatedMovie)
+    }
+  )
 })
 
 module.exports = movieRouter
